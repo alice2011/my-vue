b8a57c7ddb9038ef4dc5538d118ffe3580b5ef96
alice2011 <alice2011@sina.com>
2021-02-05 16:43:34 +0800

    订单管理页面

--- src/views/Info/index.vue
+++ src/views/Info/index.vue
@@ -58,9 +58,13 @@
             </el-col>
         </el-row>
         <el-table :data="tableData" border style="width: 100%">
-        <el-table-column prop="date" label="日期" width="180"></el-table-column>
-        <el-table-column prop="name" label="姓名" width="180"></el-table-column>
-        <el-table-column prop="address" label="地址"></el-table-column>
+        <el-table-column prop="title" label="标题"></el-table-column>
+        <el-table-column prop="category" label="类型"></el-table-column>
+        <el-table-column prop="date" label="日期"></el-table-column>
+        <el-table-column prop="user" label="管理员"></el-table-column>
+        <el-table-column label="操作">
+            
+        </el-table-column>
     </el-table>
     </div>
 </template>
@@ -93,12 +97,35 @@ export default {
         const value=ref('')
         const value2=ref('')
 
+        // 表格数据
+        const tableData = reactive([{
+            date: '2016-05-02',
+            name: '王小虎',
+            address: '上海市普陀区金沙江路 1518 弄'
+            }, 
+            {
+            date: '2016-05-04',
+            name: '王小虎',
+            address: '上海市普陀区金沙江路 1517 弄'
+            }, 
+            {
+            date: '2016-05-01',
+            name: '王小虎',
+            address: '上海市普陀区金沙江路 1519 弄'
+            }, 
+            {
+            date: '2016-05-03',
+            name: '王小虎',
+            address: '上海市普陀区金沙江路 1516 弄'
+        }])
+     
         return {
             options,
             searchOption,
             search_key,
             value,
-            value2
+            value2,
+            tableData
         }
     }
 }
